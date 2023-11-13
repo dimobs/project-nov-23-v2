d9414ee53708fb0980df035b3d5d5faec66ea6a6
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2023-11-14 00:21:17 +0200

    update

--- client/pr-nov-23v2/src/App.jsx
+++ client/pr-nov-23v2/src/App.jsx
@@ -1,6 +1,7 @@
 import UserList from './components/Administrator/UserList';
 import Footer from './components/Header-Footer/Footer';
 import Header from './components/Header-Footer/Header';
+import Registration from './components/Registration';
 import './styles.css';
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
 
             <main className="main">
             <UserList />
-            {/* <RegistrationPage /> */}
+          <Registration />
             </main>
             <Footer />
         </div>
